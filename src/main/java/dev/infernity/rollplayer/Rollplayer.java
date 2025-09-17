eeb6034f2f900aafc514eb5898a8fcbf75fb56e4
Whirling <infernity@infernity.dev>
2025-09-16 19:40:33 -0600

    allow for configuring the debug server 0 or unset is global

--- src/main/java/dev/infernity/rollplayer/Rollplayer.java
+++ src/main/java/dev/infernity/rollplayer/Rollplayer.java
@@ -21,7 +21,16 @@ public class Rollplayer extends ListenerAdapter {
         var listeners = new Listeners();
         Resources.INSTANCE.getLogger().info("Loading {} listeners.", listeners.listeners.size());
         api.addEventListener(listeners.listeners.toArray());
-        Objects.requireNonNull(api.getGuildById(1223799616915636287L)).updateCommands().addCommands(listeners.commands.toArray(new CommandData[0])).queue();
+        var debugServer = Resources.INSTANCE.getConfig().getLong("debug.testingServer", 0L);
+        if (debugServer == 0L) {
+            api.updateCommands().addCommands(listeners.commands.toArray(new CommandData[0]))
+                    .onSuccess(_ -> Resources.INSTANCE.getLogger().info("Commands initialized globally."))
+                    .queue();
+        } else {
+            Objects.requireNonNull(api.getGuildById(debugServer)).updateCommands().addCommands(listeners.commands.toArray(new CommandData[0]))
+                    .onSuccess(_ -> Resources.INSTANCE.getLogger().info("Commands initialized to server {}.", debugServer))
+                    .queue();
+        }
         super.onReady(event);
         Resources.INSTANCE.getLogger().info("Readied up!");
     }

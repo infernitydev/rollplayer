b7e0ba4d25c0d49edfd7b1cc4eb036f0bc071fe2
Whirling <infernity@infernity.dev>
2025-09-16 22:06:50 -0600

    suppress unused warnings for version field

--- src/main/java/dev/infernity/rollplayer/database/objects/UserSettings.java
+++ src/main/java/dev/infernity/rollplayer/database/objects/UserSettings.java
@@ -9,7 +9,8 @@ public class UserSettings {
     @Id
     @Column(name = "id", nullable = false)
     private Long id;
-
+    
+    @SuppressWarnings("unused") // Hibernate 7 recommends a version field for commonly changed fields, as settings will be.
     @Version
     @Column(name = "version")
     private Integer version;
@@ -18,6 +19,7 @@ public class UserSettings {
     @ColumnDefault("'d20'")
     private String defaultRoll;
 
+    @SuppressWarnings("unused") // Hibernate 7 recommends a version field for commonly changed fields, as settings will be.
     public Integer getVersion() {
         return version;
     }
